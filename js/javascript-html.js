d6fe252b88ed4169f54492345380a4877a740bd4
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-08 10:24:51 -0500

    Added javascript-html.html and .js files

---
+++ js/javascript-html.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*======= ALERT =======*/
+
+// alert("HEY, listen!");
+
+/*======= PROMPT =======*/
+
+// confirm("Wait! Listen!");
+
+ // let userChoice = confirm("Wait! Listen!");
+ //
+ // if (userChoice === true) {
+ //     console.log("Good Job!")
+ // }  else {
+ //     console.log("You have disappointed me.")
+ // }
