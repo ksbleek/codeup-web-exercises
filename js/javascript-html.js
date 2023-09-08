c8618d879b3a929798ba032e2d4712c19cb3f287
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-08 10:48:47 -0500

    Added prompt section to javascript-html.js

--- js/javascript-html.js
+++ js/javascript-html.js
@@ -4,7 +4,7 @@
 
 // alert("HEY, listen!");
 
-/*======= PROMPT =======*/
+/*======= CONFIRM =======*/
 
 // confirm("Wait! Listen!");
 
@@ -13,5 +13,20 @@
  // if (userChoice === true) {
  //     console.log("Good Job!")
  // }  else {
- //     console.log("You have disappointed me.")
+ //     console.log("You have disappointed me.");
  // }
+
+/*======= Prompt =======*/
+
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceLeft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceLeft/currentSpeed) * 60;
+
+let timeAtPlannedSpeed = (distanceLeft/plannedSpeed) * 60;
+
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed;
+
+console.log(`At your current speed you will take ${timeAtCurrentSpeed.toFixed(0)} minutes. At your planned speed you will take ${timeAtPlannedSpeed.toFixed(0)} minutes. You will save ${timeSaved.toFixed(0)} minutes by using planned speed.`);
+
