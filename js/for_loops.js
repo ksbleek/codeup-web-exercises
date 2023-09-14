6b7ef6eaeb2b20ee7e09fe76817eeddc09657b1f
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-14 13:40:28 -0500

    completed for_loops exercise.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,35 @@
 "use strict"
 
+/*===== Exercise 2 =====*/
+
 function showMultiplicationTable(num){
     for (let i = 0;i <= 10; i++)
         console.log(num * i)
 }
+
+/*===== Exercise 3 =====*/
+
+// function evenOdd(num){
+//     if (num % 2){
+//         return `${num} is even.`;
+//     } else {
+//         return `${num} is odd.`;
+//     }
+// }
+
+// for (let i = 0; i < 10; i++){
+//     let x = Math.floor(Math.random() * (201 - 20))
+//     console.log(evenOdd(x))
+// }
+
+/*===== Exercise 4 =====*/
+
+// for(let i = 1; i < 10; i++){
+//     console.log(("" + i).repeat(i))
+// }
+
+/*===== Exercise 5 =====*/
+
+// for (let i = 100; i >= 5; i -= 5){
+//     console.log(i)
+// }
