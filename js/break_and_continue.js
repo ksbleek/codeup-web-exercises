d3b9c5872753df1d051f917bdac09e02ea53b60b
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-14 14:47:38 -0500

    completed break_and_continue.js

---
+++ js/break_and_continue.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// let i = 0;
+
+// while(true){
+//     i = parseInt(prompt("Give a number between 1 and 50?"));
+//     if(i > 1 && i < 50){
+//         break;
+//     }
+// }
+// for(let j = 0; j <= 50; j++){
+//     if(j === i){
+//         continue;
+//     }
+//     console.log(j)
+// }
