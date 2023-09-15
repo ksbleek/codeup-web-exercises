a2f695246f665b861126c95d5b00909186db9e36
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-15 09:04:41 -0500

    changed allCones to = instead of >= in the while portion of loop.

--- js/while.js
+++ js/while.js
@@ -6,17 +6,17 @@
 //     console.log(i)
 //     i = i * 2;
 // }
-let allCones = Math.floor(Math.random() * 50) + 50;
-
-do{
-    let i =  Math.floor(Math.random() * 5) + 1;
-    if(i < allCones){
-        allCones = allCones - i
-        console.log(`${i} cones sold.`)
-    } else if (i > allCones){
-        console.log(`Cannot sell ${i} I only have ${allCones} left.`)
-    } else{
-        console.log(`Yay! I sold them all!`)
-        break;
-    }
-} while (allCones >= 0)
+// let allCones = Math.floor(Math.random() * 50) + 50;
+//
+// do{
+//     let i =  Math.floor(Math.random() * 5) + 1;
+//     if(i < allCones){
+//         allCones = allCones - i
+//         console.log(`${i} cones sold.`)
+//     } else if (i > allCones){
+//         console.log(`Cannot sell ${i} I only have ${allCones} left.`)
+//     } else{
+//         console.log(`Yay! I sold them all!`)
+//         break;
+//     }
+// } while (allCones = 0)
